96fccc4d3692ed48f48fd29e261286ba58168da6
iZotope235 <mmnik84@mail.ru>
2023-07-07 22:29:41 +0300

    2. Модель данных. [#174683  [#174683]]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -1,1 +1,14 @@
+package ru.job4j.pojo;
 
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student ivan = new Student();
+        ivan.setFullName("Ivan Ivanov");
+        ivan.setGroup("0512");
+        ivan.setAdmission(new Date());
+        System.out.println(ivan.getFullName() + System.lineSeparator() + ivan.getGroup()
+                + System.lineSeparator() + ivan.getAdmission());
+    }
+}

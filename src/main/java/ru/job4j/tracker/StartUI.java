59784244bf1b8bc02498fcbf50588e9ceaa6e31a
iZotope235 <mmnik84@mail.ru>
2023-07-09 18:11:37 +0300

    10. toString [#310018  [#310018]]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,15 +1,9 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item("");
-        LocalDateTime currentDateTime = item.getDate();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateFormat = currentDateTime.format(formatter);
-        System.out.println(currentDateFormat);
+        Item item = new Item();
+        System.out.println(item);
     }
 
 }

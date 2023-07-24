7df4429c941030f0538e6ffab418d72cccdca59b
iZotope235 <mmnik84@mail.ru>
2023-07-24 10:14:32 +0300

    12.2. Тесты вывода на консоль [#33585  [#33585]]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -18,7 +18,7 @@ public class StartUI {
     }
 
     private void showMenu(UserAction[] actions) {
-        out.println("Menu.");
+        out.println("Menu:");
         for (int index = 0; index < actions.length; index++) {
             out.println(index + ". " + actions[index].name());
         }
@@ -29,7 +29,7 @@ public class StartUI {
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {new CreateAction(output), new ShowAllAction(output), new ReplaceAction(output), new DeleteAction(output),
-                                 new FindByIdAction(output), new FindByNameAction(output), new Exit()};
+                                 new FindByIdAction(output), new FindByNameAction(output), new ExitAction(output)};
         new StartUI(output).init(input, tracker, actions);
     }
 }

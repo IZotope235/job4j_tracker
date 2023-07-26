cb56420e3c041ab06b9baa5a3433f022025a1f84
iZotope235 <mmnik84@mail.ru>
2023-07-26 09:06:42 +0300

    2.1. Тест на ValidateInput [#311887  [#311887]]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"one", "1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+    }
+
+    @Test
+    public void whenRepeatedValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"2", "1", "1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(2);
+    }
+
+    @Test
+    public void whenNegativeValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"-1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(-1);
+    }
+}

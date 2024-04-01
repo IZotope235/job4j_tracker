4dfc30ff640e7ef9699732ac792175cbc1550016
Marat <mmnik84@mail.ru>
2024-04-02 01:28:09 +0300

    1. Профилирование. [#1591]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -35,11 +35,12 @@ public class StartUI {
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
         Input input = new ValidateInput(output, new ConsoleInput());
-        Store tracker = new SqlTracker();
+        Store trackerMemory = new MemTracker();
         List<UserAction> actions = new ArrayList<>(Arrays.asList(new CreateAction(output),
                 new DeleteAction(output), new FindByIdAction(output), new FindByNameAction(output),
-                new ReplaceAction(output), new FindAllAction(output), new ExitAction(output)));
-        new StartUI(output).init(input, tracker, actions);
+                new ReplaceAction(output), new FindAllAction(output), new ExitAction(output), new CreateManyItems(output),
+                new DeleteAllItems(output)));
+        new StartUI(output).init(input, trackerMemory, actions);
     }
 }
 
